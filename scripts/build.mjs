8ae9ab87f9df7a3d1ea39d1c64ba42cf0c835f7c
Michal Svorc <dev@michalsvorc.com>
2023-02-07 20:22:18 +0000

    build(build): remove console log, refactoring

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,25 +1,22 @@
 /* eslint-disable node/no-unpublished-import */
-/* eslint-disable no-console */
 import { execa } from "execa";
-import logger from "./utils/logger.mjs";
+import { logger } from "./utils/logger.mjs";
 import tsconfig from "../tsconfig.build.json" assert { type: "json" };
 
 const env = {
   NODE_ENV: process.env.NODE_ENV ?? "pruduction",
 };
-const options = { env };
+const executionOptions = { env };
 const { outDir } = tsconfig.compilerOptions;
 
 try {
   if (env.NODE_ENV === "production") {
-    console.info("Cleaning directories:", outDir);
-    const clean = await execa("del-cli", [outDir], options);
-    logger(clean);
+    logger({ stdout: `Cleaning directories: ${outDir}` });
+    await execa("del-cli", [outDir], executionOptions);
   }
 
-  console.info("Build env:", env);
-  const compile = await execa("yarn", ["compile"], options);
-  logger(compile);
+  logger({ stdout: `Build env: ${JSON.stringify(env)}` });
+  await execa("yarn", ["compile"], executionOptions);
 } catch (error) {
-  console.error(error);
+  logger({ stderr: error });
 }
