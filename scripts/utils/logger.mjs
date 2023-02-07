ca19ce0fb858579c84fdf9e0371ef0c55e22542c
Michal Svorc <dev@michalsvorc.com>
2023-02-07 20:21:37 +0000

    build(logger): refactoring

--- scripts/utils/logger.mjs
+++ scripts/utils/logger.mjs
@@ -1,4 +1,10 @@
 /* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-export default ({ stdout, stderr }) =>
-  (stdout || stderr) && console.info(stdout, stderr);
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+export function logger({ stdout, stderr } = {}) {
+  Boolean(stdout) && console.info(stdout);
+  Boolean(stderr) && console.error(stderr);
+
+  if (!stdout && !stderr) {
+    console.error("Logger: No logging payload provided.");
+  }
+}
