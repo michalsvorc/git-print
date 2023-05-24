49a600ad133a2c4db79a40ed0a5688a9b5e2babf
Michal Svorc <dev@michalsvorc.com>
2023-05-24 14:21:31 +0000

    refactor(output): resolve single file to  abs path

--- src/output/resolveAbsolutePaths.ts
+++
@@ -1,8 +0,0 @@
-import * as path from "path";
-
-export function resolveAbsolutePaths(
-  cwd: string,
-  filenames: readonly string[]
-) {
-  return filenames.map((filename) => path.resolve(cwd, filename));
-}
