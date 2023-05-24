a22fd2dc534ed4dc3361845324bb116d39ac5b7b
Michal Svorc <dev@michalsvorc.com>
2023-05-24 16:21:18 +0000

    refactor(commands): get git root error handling

--- src/commands/getGitRoot.spec.ts
+++ src/commands/getGitRoot.spec.ts
@@ -5,7 +5,7 @@ import { getGitRoot } from "./getGitRoot.js";
 vi.mock("execa");
 
 describe("Get git root", () => {
-  it("should return full path to the root directory with .git repository data", async () => {
+  it("should return full path to the parent directory with .git/", async () => {
     const command = "git";
     const args = ["rev-parse", "--show-toplevel"];
     const options = undefined;
--- src/commands/getGitRoot.ts
+++ src/commands/getGitRoot.ts
@@ -1,9 +1,15 @@
 import type { ExecaReturnValue } from "execa";
 import { execute } from "./execute.js";
 
-export function getGitRoot(): Promise<ExecaReturnValue> {
+export async function getGitRoot(): Promise<ExecaReturnValue> {
   const command = "git";
   const args = ["rev-parse", "--show-toplevel"];
 
-  return execute(command)(args);
+  try {
+    return await execute(command)(args);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Searching for git directory root failed.");
+    throw error;
+  }
 }
